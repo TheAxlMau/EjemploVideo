5d65a16a0f48d8c991f3608960d59453eef1ac5d
Axl <theaxllay@gmail.com>
2024-10-02 19:24:33 -0500

    Se genero el proyecto mainV1.2

--- EjemploVideo/EjemploVideo/Form1.Designer.cs
+++ EjemploVideo/EjemploVideo/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(342, 73);
             this.label1.TabIndex = 0;
-            this.label1.Text = "MAIN V1.1";
+            this.label1.Text = "MAIN V1.2";
             // 
             // button1
             // 
